317fb9807162c6b9d1b6da6ffd08364ebc16433a
bluekmky1 <115731932+bluekmky1@users.noreply.github.com>
2024-08-16 15:24:05 +0900

    🎨 데이터 베이스 변경으로 인한 model 변경

--- lib/domain/bus_stop/model/bus_stop_model.dart
+++ lib/domain/bus_stop/model/bus_stop_model.dart
@@ -2,21 +2,24 @@ class BusStopModel {
   String stopId;
   String stopName;
   int cityCode;
+  double lat;
+  double long;
   String cityName;
-  DateTime dataCollectionDate;
 
   BusStopModel({
     required this.stopId,
     required this.stopName,
     required this.cityCode,
     required this.cityName,
-    required this.dataCollectionDate,
+    required this.lat,
+    required this.long,
   });
   factory BusStopModel.fromSupabase(Map<String, dynamic> data) => BusStopModel(
         stopId: data['stopId'] as String,
         stopName: data['stopName'] as String,
         cityCode: data['cityCode'] as int,
+        lat: data['lat'] as double,
+        long: data['long'] as double,
         cityName: data['cityName'] as String,
-        dataCollectionDate: DateTime.parse(data['dataCollectionDate']),
       );
 }
