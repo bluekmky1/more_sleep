e6734105e5709b604fa4afe633d6c729227270f0
bluekmky1 <115731932+bluekmky1@users.noreply.github.com>
2024-08-18 14:06:09 +0900

    🎨 정류장 모델에 Equtable 적용 init 함수 추가

--- lib/domain/bus_stop/model/bus_stop_model.dart
+++ lib/domain/bus_stop/model/bus_stop_model.dart
@@ -1,12 +1,14 @@
-class BusStopModel {
-  String stopId;
-  String stopName;
-  int cityCode;
-  double lat;
-  double long;
-  String cityName;
+import 'package:equatable/equatable.dart';
 
-  BusStopModel({
+class BusStopModel extends Equatable {
+  final String stopId;
+  final String stopName;
+  final int cityCode;
+  final double lat;
+  final double long;
+  final String cityName;
+
+  const BusStopModel({
     required this.stopId,
     required this.stopName,
     required this.cityCode,
@@ -14,6 +16,14 @@ class BusStopModel {
     required this.lat,
     required this.long,
   });
+  const BusStopModel.init()
+      : stopId = '',
+        stopName = '',
+        cityCode = 0,
+        cityName = '',
+        lat = 0,
+        long = 0;
+
   factory BusStopModel.fromSupabase(Map<String, dynamic> data) => BusStopModel(
         stopId: data['stopId'] as String,
         stopName: data['stopName'] as String,
@@ -22,4 +32,14 @@ class BusStopModel {
         long: data['long'] as double,
         cityName: data['cityName'] as String,
       );
+
+  @override
+  List<Object?> get props => <Object?>[
+        stopId,
+        stopName,
+        cityCode,
+        lat,
+        long,
+        cityName,
+      ];
 }
