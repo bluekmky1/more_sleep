684118992ed9d47eb2164efb61f692c72dab3b88
bluekmky1 <115731932+bluekmky1@users.noreply.github.com>
2024-08-18 14:09:43 +0900

    🎨 재탐색 버튼 클릭 시 코드

--- lib/ui/home/widgets/research_button_widget.dart
+++ lib/ui/home/widgets/research_button_widget.dart
@@ -1,15 +1,21 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../theme/color_theme.dart';
 
-class ResearchButtonWidget extends StatelessWidget {
+class ResearchButtonWidget extends ConsumerWidget {
   const ResearchButtonWidget({
+    required this.onpressed,
     super.key,
   });
 
+  final AsyncCallback onpressed;
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final ColorTheme colorTheme = Theme.of(context).extension<ColorTheme>()!;
+
     return Positioned(
       top: 65,
       right: 0,
@@ -23,7 +29,7 @@ class ResearchButtonWidget extends StatelessWidget {
                   elevation: 2,
                   shadowColor: colorTheme.surface75,
                   backgroundColor: colorTheme.backgroundElevated),
-              onPressed: () {},
+              onPressed: onpressed,
               child: const Text('현 위치에서 재검색'),
             ),
           ],
